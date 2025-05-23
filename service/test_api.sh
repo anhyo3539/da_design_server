998b68b23d2b064b4f73fe2f76cab297b40353ce
cloudhyo <acg030809@naver.com>
2025-05-23 15:52:12 +0900

    da_designserver거의 완성

--- service/test_api.sh
+++ service/test_api.sh
@@ -2,6 +2,10 @@ if [ $# -lt 1 ]; then
         echo 'give 1 argument'
 else
         if [ $1 = 'list' ]; then
-                curl -H "Content-type: application/json" -X POST -d '{"top_k":"10"}' http://203.255.81.132:10113/api-list >> ret
+                curl -H "Content-type: application/json" -X POST -d '{"top_k":"10"}' http://192.168.0.10:10113/api-list >> ret
         fi
+        if [ $1 = 'predict' ]; then
+                curl -H "Content-type: application/json" -X POST -d '{"company_name":"삼성전자"}' http://192.168.0.10:10113/api-predict >> ret
+        fi
+
 fi
